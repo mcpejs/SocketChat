51d788f4befad8ffc48edb6fb86b0be861140969
mcpejs <mcpejs1@gmail.com>
2020-02-15 16:52:15 +0900

    주석및 줄바꿈 추가

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 var app = require('express')()
 var server = require('http').createServer(app)
 var io = require('socket.io')(server)
+var socketMethod = require('./routes/socket')
 
+// ejs 사용
 app.set('view engine', 'ejs');
 
 // 라우팅
 app.use('/',require('./routes/index'))
 
-// 소켓 서버
-require('./routes/socket')(io) 
+// 소켓적용
+socketMethod(io) 
 
 server.listen(3000)
